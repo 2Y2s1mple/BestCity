f4b7e04b50843b2a0a11e534e686143ffb16e962
JasonBourne <1219709884@qq.com>
2019-02-18 18:40:15 +0800

    完成了更换手机的前两步需求

--- BestCity/Classes/Me-我的/MyProfile-我的资料/Administration-账号管理/CZAdministratorAccountController.m
+++ BestCity/Classes/Me-我的/MyProfile-我的资料/Administration-账号管理/CZAdministratorAccountController.m
@@ -8,8 +8,16 @@
 
 #import "CZAdministratorAccountController.h"
 #import "CZNavigationView.h"
-@interface CZAdministratorAccountController ()
+#import "CZSaveTool.h"
+#import "CZCZChangeMobileController.h"
 
+@interface CZAdministratorAccountController ()
+/** 手机号更换按钮 */
+@property (nonatomic, weak) IBOutlet UIButton *numberChangeBtn;
+/** 手机号 */
+@property (nonatomic, weak) IBOutlet UILabel *phoneLabel;
+/** 微信更换按钮 */
+@property (nonatomic, weak) IBOutlet UIButton *weixinChangeBtn;
 @end
 
 @implementation CZAdministratorAccountController
@@ -19,16 +27,26 @@
     //导航条
     CZNavigationView *navigationView = [[CZNavigationView alloc] initWithFrame:CGRectMake(0, (IsiPhoneX ? 24 : 0), SCR_WIDTH, 67) title:@"账号管理" rightBtnTitle:nil rightBtnAction:nil navigationViewType:CZNavigationViewTypeBlack];
     [self.view addSubview:navigationView];
+    
+    // 用户信息
+    NSDictionary *userInfo = [CZSaveTool objectForKey:@"user"];
+    self.phoneLabel.text = userInfo[@"mobile"];
+    if ([userInfo[@"bindWeixin"]  isEqual: @"1"]) { // 已绑定
+        [self.weixinChangeBtn setBackgroundColor:CZREDCOLOR];
+        [self.weixinChangeBtn setTitle:@"已绑定" forState:UIControlStateNormal];
+        [self.weixinChangeBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    } else {
+        [self.weixinChangeBtn setBackgroundColor:[UIColor whiteColor]];
+        [self.weixinChangeBtn setTitle:@"去绑定" forState:UIControlStateNormal];
+        [self.weixinChangeBtn setTitleColor:CZGlobalGray forState:UIControlStateNormal];
+    }
+        
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)changePhoneNumber
+{
+    CZCZChangeMobileController *vc = [[CZCZChangeMobileController alloc] init];
+    [self.navigationController pushViewController:vc animated:YES];
 }
-*/
 
 @end
---
+++ BestCity/Classes/Me-我的/MyProfile-我的资料/Administration-账号管理/CZCZChangeMobileController.m
@@ -0,0 +1,136 @@
+//
+//  CZCZChangeMobileController.m
+//  BestCity
+//
+//  Created by JasonBourne on 2019/2/18.
+//  Copyright © 2019 JasonBourne. All rights reserved.
+//
+
+#import "CZCZChangeMobileController.h"
+#import "CZNavigationView.h"
+#import "GXNetTool.h"
+#import "CZBindingMobileController.h"
+
+@interface CZCZChangeMobileController ()
+/** 当前手机号 */
+@property (nonatomic, weak) IBOutlet UILabel
+*phoneNumber;
+
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+/** 验证码 */
+@property (weak, nonatomic) IBOutlet UIButton *verificationCodeBtn;
+/** 立即更换 */
+@property (weak, nonatomic) IBOutlet UIButton *loginBtn;
+/** 定时器 */
+@property (nonatomic, strong) NSTimer *timer;
+/** 时间总数 */
+@property (nonatomic, assign) NSInteger count;
+/** 判断是否在读秒 */
+@property (nonatomic, assign) BOOL isReadSecond;
+@end
+
+@implementation CZCZChangeMobileController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+    //导航条
+    CZNavigationView *navigationView = [[CZNavigationView alloc] initWithFrame:CGRectMake(0, 0, SCR_WIDTH, 67) title:@"更换手机号" rightBtnTitle:nil rightBtnAction:nil navigationViewType:CZNavigationViewTypeBlack];
+    navigationView.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:navigationView];
+    [self enabledAndRedColor:self.verificationCodeBtn];
+    [self.passwordTextField addTarget:self action:@selector(textFieldAction:) forControlEvents:UIControlEventEditingChanged];
+    
+    // 用户信息
+    NSDictionary *userInfo = [CZSaveTool objectForKey:@"user"];
+    self.phoneNumber.text = userInfo[@"mobile"];
+}
+
+- (void)textFieldAction:(UITextField *)textField
+{
+    
+    if (self.passwordTextField.text.length != 0) {
+        [self enabledAndRedColor:self.loginBtn];
+    } else {
+        [self disabledAndGrayColor:self.loginBtn];
+    }
+}
+
+#pragma mark - 获取验证码
+- (IBAction)getVerificationCode:(id)sender {
+    [self disabledAndGrayColor:self.verificationCodeBtn];
+    self.count = 60;
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(timeDown) userInfo:nil repeats:YES];
+    
+    // 发送验证码
+    NSMutableDictionary *param = [NSMutableDictionary dictionary];
+    param[@"mobile"] = self.phoneNumber.text;
+    param[@"type"] = @(3); // 3更换手机号
+    
+    NSString *url = [JPSERVER_URL stringByAppendingPathComponent:@"api/sendMessage"];
+    
+    [GXNetTool PostNetWithUrl:url body:param bodySytle:GXRequsetStyleBodyHTTP header:nil response:GXResponseStyleJSON success:^(id result) {
+        if ([result[@"msg"] isEqualToString:@"success"]) {
+            [CZProgressHUD showProgressHUDWithText:@"验证码发送成功"];
+        } else {
+            [CZProgressHUD showProgressHUDWithText:@"验证码发送失败"];
+        }
+        [CZProgressHUD hideAfterDelay:2];
+    } failure:^(NSError *error) {
+        
+    }];
+}
+
+- (void)timeDown
+{
+    // 是否在读秒
+    self.isReadSecond = YES;
+    self.count -= 1;
+    [self.verificationCodeBtn setTitle:[NSString stringWithFormat:@"%lds后重发", (long)self.count] forState:UIControlStateNormal];
+    if (_count == 0) {
+        [self.timer invalidate];
+        [self enabledAndRedColor:self.verificationCodeBtn];
+        [self.verificationCodeBtn setTitle:@"获取验证码" forState:UIControlStateNormal];
+        self.isReadSecond = NO;
+    }
+    
+}
+
+#pragma mark - 立即更换
+- (IBAction)nextAction:(UIButton *)sender {
+    sender.enabled = NO;
+    // 验证
+    NSMutableDictionary *param = [NSMutableDictionary dictionary];
+    param[@"code"] = self.passwordTextField.text;
+    
+    
+    NSString *url = [JPSERVER_URL stringByAppendingPathComponent:@"api/user/validateMobileCode"];
+    
+    [GXNetTool PostNetWithUrl:url body:param bodySytle:GXRequsetStyleBodyHTTP header:nil response:GXResponseStyleJSON success:^(id result) {
+        sender.enabled = YES;
+        if ([result[@"msg"] isEqualToString:@"success"]) {
+            CZBindingMobileController *vc = [[CZBindingMobileController alloc] init];
+            [self.navigationController pushViewController:vc animated:YES];
+        } else {
+            [CZProgressHUD showProgressHUDWithText:@"验证码错误"];
+        }
+        [CZProgressHUD hideAfterDelay:2];
+    } failure:^(NSError *error) {
+        
+    }];}
+
+/** 激活红色 */
+- (void)enabledAndRedColor:(UIButton *)btn
+{
+    btn.backgroundColor = CZREDCOLOR;
+    btn.enabled = YES;
+}
+
+/** 残疾灰色 */
+- (void)disabledAndGrayColor:(UIButton *)btn
+{
+    btn.backgroundColor = CZBTNGRAY;
+    btn.enabled = NO;
+}
+
+@end
